bb0990e3f750b4934ea9b8a789484eeb38046260
clickingmouse <clickingmouse@users.noreply.github.com>
2019-10-28 10:09:14 +0800

    debugging heroku

--- server.js
+++ server.js
@@ -21,14 +21,15 @@ const authRoutes = require("./routes/api/authRoutes");
 //app.use(bodyParser.json());
 
 //app.use(cors());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
   bodyParser.urlencoded({
